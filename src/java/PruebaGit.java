2772fea9c9acb0c9247ed8c47a4d012f9a27fee4
andres2397 <aajm1997@gmail.com>
2017-05-18 16:38:08 -0600

    Update PruebaGit.java
    prueba 2

--- src/java/PruebaGit.java
+++ src/java/PruebaGit.java
@@ -16,6 +16,7 @@ public class PruebaGit {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Prueba de navegador a netbeans");
+         System.out.println(" netbeans");
 
     
 
